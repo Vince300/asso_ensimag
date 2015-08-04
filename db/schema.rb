04a7521424520fe6c7822f2679fee7654d7841c2
Vincent Tavernier <vincent.tavernier@ensimag.grenoble-inp.fr>
2015-08-04 22:46:29 +0200

    Database edits, rake db:reset needed

--- db/schema.rb
+++ db/schema.rb
@@ -96,7 +96,6 @@ ActiveRecord::Schema.define(version: 20150804175639) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  add_index "users", ["slug"], name: "index_users_on_slug"
-  add_index "users", ["username"], name: "index_users_on_username", unique: true
+  add_index "users", ["slug"], name: "index_users_on_slug", unique: true
 
 end
