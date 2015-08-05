5f7a755d7e9ddb2421e514dc084faec62b088900
Vincent Tavernier <vincent.tavernier@ensimag.grenoble-inp.fr>
2015-08-05 02:23:02 +0200

    Updated seed loading code

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,21 @@
 require 'active_record/fixtures'
 
+# The list of classes to load
+class_names = [ :admin_users, :blog_posts, :events, :users ]
+
+# Find out the environment
 e = ENV['FIXTURES_ENV'] || Rails.env
-if e == "test"
+if e == "test" and not Dir.exists? Rails.root.join('db', 'seeds', 'test')
   e = "development"
 end
 
-r = Rails.root.join('db', 'fixtures', e)
-ActiveRecord::FixtureSet.create_fixtures(r, Dir[r + "/*.yml"])
+# Get the data directory
+r = Rails.root.join('db', 'seeds', e)
+
+# Load the fixtures
+ActiveRecord::FixtureSet.create_fixtures(r, class_names)
+
+# Apply save method to all classes
+class_names.map { |s| s.to_s.classify.constantize } .each do |cls|
+  cls.find_each(&:save)
+end
