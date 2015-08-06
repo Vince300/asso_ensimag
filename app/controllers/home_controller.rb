1921acab179fdff051b5c70cceaa440476febed5
Vincent Tavernier <vincent.tavernier@ensimag.grenoble-inp.fr>
2015-08-06 15:56:01 +0200

    Bug fixes in home/index view and controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,6 @@ class HomeController < ApplicationController
   def index
     @posts = BlogPost.limit(5)
     @assos = User.sample(3)
-    @events = Event.coming.limit(2)
+    @events = Event.coming.limit(3)
   end
 end
