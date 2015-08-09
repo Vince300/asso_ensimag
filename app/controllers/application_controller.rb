f948500eb2303a45fbc9746640876464a281f8f9
Vincent Tavernier <vincent.tavernier@ensimag.grenoble-inp.fr>
2015-08-09 02:28:03 +0200

    Added general error handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,13 @@ class ApplicationController < ActionController::Base
 
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
+  # Error handlers
+  unless Rails.application.config.consider_all_requests_local
+    rescue_from Exception, with: :error_internal
+    rescue_from Pundit::NotAuthorizedError, with: :error_forbidden
+    rescue_from ActiveRecord::RecordNotFound, ActionController::RoutingError, with: :error_not_found
+  end
+
   protected
     #->Prelang (user_login:devise)
     def configure_permitted_parameters
@@ -18,6 +25,28 @@ class ApplicationController < ActionController::Base
     def verify_pundit
       unless request.original_fullpath =~ /^\/admin/ or devise_controller?
         verify_authorized
+      end
+    end
+
+    def error_forbidden
+      @error_status = 403
+      render_error
+    end
+
+    def error_not_found
+      @error_status = 404
+      render_error
+    end
+
+    def error_internal
+      @error_status = 500
+      render_error
+    end
+
+    def render_error
+      respond_to do |format|
+        format.html { render 'errors/error', status: @error_status }
+        format.all { render nothing: true, status: @error_status }
       end
     end
 end
