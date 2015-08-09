426de00d495c3fc7a1a81b1d200839ab60a79c12
Vincent Tavernier <vincent.tavernier@ensimag.grenoble-inp.fr>
2015-08-09 22:15:13 +0200

    Fixed deployment scripts

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,29 +26,21 @@ set :deploy_to, '/srv/asso-ensimag'
 # set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 
 # Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp', 'vendor/bundle', 'public/uploads')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
 # Default value for keep_releases is 5
-# set :keep_releases, 5
+set :keep_releases, 3
 
-namespace :deploy do
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
-  end
-
-end
+# Fixes for mixed platform Gemfile.lock
+set :bundle_flags, ''
 
 task :remove_gemfile_lock do
-  run "rm -f #{release_path}/Gemfile.lock"
+  on roles(:all) do
+    execute "rm -f #{release_path}/Gemfile.lock"
+  end
 end
 
 before "bundler:install", "remove_gemfile_lock"
